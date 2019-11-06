41f970fc7cd005972ac249dddf422e1e835df75f
SravanKumarN <sravankumar.n@hotmail.com>
2019-11-06 00:19:41 -0600

    truthy and falsy varibles

---
+++ truthyVsfalsy.js
@@ -0,0 +1,15 @@
+/************************************************
+ * 
+ * falsy values: undefined, NaN, null, 0, ''
+ * truthy values: !truthy
+ * 
+ ***********************************************/
+
+ let iAm;
+
+ if (iAm) {
+     console.log('i am defined and truthy :-)');
+ } else {
+     console.log('i am falsy :-(')
+ }
+ 
